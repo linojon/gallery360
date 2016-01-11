b767254ccffe02731139abdc9fd3f3cb4a4decd7
Jonathan Linowes <jonathan@parkerhill.com>
2016-01-11 17:08:50 -0500

    first commit

---
+++ app/src/main/java/com/cardbookvr/gallery360/MainActivity.java
@@ -0,0 +1,70 @@
+package com.cardbookvr.gallery360;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.cardbook.renderbox.IRenderBox;
+import com.cardbook.renderbox.RenderBox;
+import com.cardbook.renderbox.Transform;
+import com.cardbook.renderbox.components.Camera;
+import com.cardbook.renderbox.components.Sphere;
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+import com.google.vrtoolkit.cardboard.CardboardView;
+
+public class MainActivity extends CardboardActivity implements IRenderBox {
+    final String TAG = "MainActivity";
+
+    final int DEFAULT_PHOTO = R.drawable.sample360;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CardboardView cardboardView = (CardboardView) findViewById(R.id.cardboard_view);
+        cardboardView.setRestoreGLStateEnabled(false);
+        cardboardView.setRenderer(new RenderBox(this, this));
+        setCardboardView(cardboardView);
+    }
+
+    @Override
+    public void setup() {
+        Transform background = new Transform();
+        background.setParent(RenderBox.mainCamera.transform, false);
+        background.setLocalScale(-Camera.Z_FAR, -Camera.Z_FAR, -Camera.Z_FAR);
+        background.addComponent(new Sphere(DEFAULT_PHOTO, false));
+    }
+
+    @Override
+    public void preDraw() {
+
+    }
+
+    @Override
+    public void postDraw() {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
