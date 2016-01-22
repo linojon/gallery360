b388733438006ac1b491ad2afeb5afffd8a6e3b5
Jonathan Linowes <jonathan@parkerhill.com>
2016-01-22 18:49:33 -0500

    image loadTexture and rotate

--- app/src/main/java/com/cardbookvr/gallery360/Image.java
+++ app/src/main/java/com/cardbookvr/gallery360/Image.java
@@ -1,6 +1,17 @@
 package com.cardbookvr.gallery360;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ExifInterface;
+import android.opengl.GLES20;
+import android.opengl.GLUtils;
 import android.util.Log;
+
+import com.cardbook.renderbox.RenderBox;
+import com.cardbook.renderbox.math.Quaternion;
+import com.google.vrtoolkit.cardboard.CardboardView;
+
+import java.io.IOException;
 
 /**
  * Created by Jonathan on 1/16/2016.
@@ -8,6 +19,8 @@ import android.util.Log;
 public class Image {
     final static String TAG = "image";
     String path;
+    int textureHandle;
+    Quaternion rotation;
 
     public Image(String path) {
         this.path = path;
@@ -24,5 +37,96 @@ public class Image {
         if(split== null || split.length < 2)
             return null;
         return split[split.length - 1].toLowerCase();
+    }
+
+    public void loadTexture(CardboardView cardboardView){
+        final Bitmap bitmap = BitmapFactory.decodeFile(path);
+        if(bitmap == null){
+            throw new RuntimeException("Error loading bitmap.");
+        }
+        textureHandle = bitmapToTexture(bitmap);
+    }
+
+    public static int bitmapToTexture(Bitmap bitmap){
+        final int[] textureHandle = new int[1];
+
+        GLES20.glGenTextures(1, textureHandle, 0);
+        RenderBox.checkGLError("Bitmap GenTexture");
+
+        if (textureHandle[0] != 0) {
+            // Bind to the texture in OpenGL
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureHandle[0]);
+
+            // Set filtering
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_NEAREST);
+
+            // Load the bitmap into the bound texture.
+            GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0);
+        }
+        if (textureHandle[0] == 0){
+            throw new RuntimeException("Error loading texture.");
+        }
+
+        return textureHandle[0];
+    }
+
+    public void show(CardboardView cardboardView, Plane screen) {
+        BorderMaterial material = (BorderMaterial) screen.getMaterial();
+        loadTexture(cardboardView);
+        material.setTexture(textureHandle);
+        calcRotation();
+        if (rotation != null)
+            screen.transform.setLocalRotation(new Quaternion(rotation));
+    }
+
+    void calcRotation(){
+        rotation = null;
+
+        // use Exif tags to determine orientation, only available in jpg (and jpeg)
+        String ext = getExtension(path);
+        if (!ext.equals("jpg") && !ext.equals("jpeg"))
+            return;
+
+        try {
+            ExifInterface exif = new ExifInterface(path);
+            //height = exif.getAttribute(ExifInterface.TAG_I);
+            //rotation = exif.getAttribute(ExifInterface.TAG_ORIENTATION);
+            switch (exif.getAttribute(ExifInterface.TAG_ORIENTATION)){
+                // Correct orientation, but flipped on the horizontal axis
+                case "2":
+                    rotation = new Quaternion().setEulerAngles(180,0,0);
+                    break;
+                // Upside-down
+                case "3":
+                    rotation = new Quaternion().setEulerAngles(0,0,180);
+                    break;
+                // Upside-Down & Flipped along horizontal axis
+                case "4":
+                    rotation = new Quaternion().setEulerAngles(180,0,180);
+                    break;
+                // Turned 90 deg to the left and flipped
+                case "5":
+                    rotation = new Quaternion().setEulerAngles(0,180,90);
+                    break;
+                // Turned 90 deg to the left
+                case "6":
+                    rotation = new Quaternion().setEulerAngles(0,0,-90);
+                    break;
+                // Turned 90 deg to the right and flipped
+                case "7":
+                    rotation = new Quaternion().setEulerAngles(0,180,90);
+                    break;
+                // Turned 90 deg to the right
+                case "8":
+                    rotation = new Quaternion().setEulerAngles(0,0,90);
+                    break;
+                //Correct orientation--do nothing
+                default:
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/cardbookvr/gallery360/MainActivity.java
+++ app/src/main/java/com/cardbookvr/gallery360/MainActivity.java
@@ -7,7 +7,6 @@ import com.cardbook.renderbox.IRenderBox;
 import com.cardbook.renderbox.RenderBox;
 import com.cardbook.renderbox.Transform;
 import com.cardbook.renderbox.components.Camera;
-import com.cardbook.renderbox.components.RenderObject;
 import com.cardbook.renderbox.components.Sphere;
 import com.google.vrtoolkit.cardboard.CardboardActivity;
 import com.google.vrtoolkit.cardboard.CardboardView;
@@ -24,12 +23,15 @@ public class MainActivity extends CardboardActivity implements IRenderBox {
 
     final List<Image> images = new ArrayList<>();
 
+    CardboardView cardboardView;
+    Plane screen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CardboardView cardboardView = (CardboardView) findViewById(R.id.cardboard_view);
+        cardboardView = (CardboardView) findViewById(R.id.cardboard_view);
         cardboardView.setRestoreGLStateEnabled(false);
         cardboardView.setRenderer(new RenderBox(this, this));
         setCardboardView(cardboardView);
@@ -39,26 +41,31 @@ public class MainActivity extends CardboardActivity implements IRenderBox {
     public void setup() {
         setupBackground();
         setupScreen();
-        getImageList(imagesPath);
+        loadImageList(imagesPath);
+        showImage(images.get(0));
     }
 
     void setupBackground() {
-        Transform background = new Transform()
+        new Transform()
                 .setLocalScale(-Camera.Z_FAR, -Camera.Z_FAR, -Camera.Z_FAR)
                 .addComponent(new Sphere(DEFAULT_PHOTO, false));
     }
 
     void setupScreen() {
         BorderMaterial screenMaterial = new BorderMaterial();
-        screenMaterial.setTexture(RenderObject.loadTexture(R.drawable.sample360));
-        Plane screen = new Plane();
+        //screenMaterial.setTexture(RenderObject.loadTexture(R.drawable.sample360));
+        screen = new Plane();
         screen.setupBorderMaterial(screenMaterial);
 
-        Transform screenTrans = new Transform()
+        new Transform()
                 .setLocalScale(4, 4, 1)
                 .setLocalPosition(0, 0, 5)
                 .setLocalRotation(0, 0, 180)
                 .addComponent(screen);
+    }
+
+    void showImage(Image image) {
+        image.show(cardboardView, screen);
     }
 
     @Override
@@ -71,7 +78,7 @@ public class MainActivity extends CardboardActivity implements IRenderBox {
 
     }
 
-    int getImageList(String path) {
+    int loadImageList(String path) {
         File f = new File(path);
         File file[] = f.listFiles();
         Log.d(TAG, "!!!!!!!!!!!!!!!!!!!!!!!!!!!");
